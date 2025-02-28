1e6d89c2f7b26d7841fad09ff8c667c0df24f526
SachinMagar <budhathokisachin143@gmail.com>
2025-02-28 08:50:36 +0545

    url change

--- sel.py
+++ sel.py
@@ -1,10 +1,10 @@
+# import necessary module for chrome browser
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# Initialize WebDriver 
-# service = Service(ChromeDriverManager().install())
+#for microsoft edge
 # import time
 # from selenium import webdriver
 # from selenium.webdriver.edge.service import Service as EdgeService
@@ -12,12 +12,13 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 # driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
 
 # Open a webpage to test
-url="https://www.mindrisers.com.np/"
+url="https://merolagani.com/"
 driver.get(url)
-
-# Close the browser after a short delay
+time.sleep(3)
+#maximize the window as selenium always open the browser in a half window
 driver.maximize_window()
-title=driver.title
+title=driver.title #just to print the title of the webpage
 print(title)
+# Close the browser after a short delay
 time.sleep(5)
 driver.quit()
