b1be58b43f438822483854e57e218e159ef663ca
SachinMagar <budhathokisachin143@gmail.com>
2025-03-10 15:35:03 +0545

    merolagani fixed

--- meroLagani.py
+++ meroLagani.py
@@ -1,51 +1,58 @@
 #script to access to the live trading market and chsing a random company to check its price history in mero lagani 
+# Importing required libraries
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.alert import Alert
+
+# Setting up Chrome WebDriver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# Open a webpage to test
-url="https://merolagani.com/"
-driver.get(url)
+
+# Open Mero Lagani
+driver.get("https://merolagani.com/")
 driver.maximize_window()
 time.sleep(2)
-market=driver.find_element(*(By.XPATH,"//a[normalize-space()='Market']"))
-market.click()
+
+# Navigate to Market & Live Trading
+driver.find_element(By.XPATH, "//a[normalize-space()='Market']").click()
 time.sleep(2)
-# alert=Alert(driver)
-# alert.accept()
+
+# Handling pop-up alerts
 try:
     alert = Alert(driver)
-    alert.dismiss()  # Dismiss the pop-up
+    alert.dismiss()
 except:
     print("No alert found")
-live=driver.find_element(*(By.XPATH,"//a[normalize-space()='Live Trading']"))
-live.click()
+
+driver.find_element(By.XPATH, "//a[normalize-space()='Live Trading']").click()
 time.sleep(2)
+
+# Scroll down & Open a company
 driver.execute_script("window.scrollBy(0,400);")
+driver.find_element(By.XPATH, "//a[normalize-space()='ADBL']").click()
 time.sleep(2)
-openLive=driver.find_element(*(By.XPATH,"//a[normalize-space()='ADBL']"))
-openLive.click()
-time.sleep(2)
-#new tab opened so need to switch the mouse to the new tab 
-driver.switch_to.window(driver.window_handles[1])  # Move to the new tab
-time.sleep(2)
-####
+
+# Switch to the new tab
+driver.switch_to.window(driver.window_handles[1])
+
+# Scroll & Access Price History
 driver.execute_script("window.scrollBy(0,950);")
-priceHistory=driver.find_element(*(By.XPATH,"//a[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_lnkHistoryTab']"))
-priceHistory.click()
+driver.find_element(By.XPATH, "//a[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_lnkHistoryTab']").click()
 time.sleep(2)
-date=driver.find_element(*(By.XPATH,"//input[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_txtMarketDatePriceFilter']"))
-date.send_keys("02/13/2023")
-time.sleep(2)
-search=driver.find_element(*(By.XPATH,"//a[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_lbtnSearchPriceHistory']"))
+
+# Enter date & search price history
+date_input = driver.find_element(By.XPATH, "//input[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_txtMarketDatePriceFilter']")
+date_input.send_keys("02/13/2022")
+
+search = driver.find_element(By.XPATH, "//a[@id='ctl00_ContentPlaceHolder1_CompanyDetail1_lbtnSearchPriceHistory']")
 search.click()
 time.sleep(2)
+
+# Scroll & View Results
 driver.execute_script("window.scrollBy(0,100);")
-# Close the browser after a short delay
+
+# Closing browser
 time.sleep(5)
 driver.quit()
-
